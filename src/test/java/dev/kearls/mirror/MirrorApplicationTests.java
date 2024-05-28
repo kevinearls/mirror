9a7b372d2bf9d879bd38d46ddc7dec214a730edd
Kevin Earls <kevinearls@mac.com>
2024-05-28 11:47:13 +0200

    Initial commit
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

---
+++ src/test/java/dev/kearls/mirror/MirrorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kearls.mirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
